efdcb0d0245b43f22b1f818813937324d3b3d9ca
Muhammad Qalbu Dary <9desimeter@gmail.com>
2024-11-19 16:25:50 +0800

    Express Server - Postman

--- 18. ExpressJS/3.2-HTTP-Requests/3.2 HTTP Requests/index.js
+++ 18. ExpressJS/3.2-HTTP-Requests/3.2 HTTP Requests/index.js
@@ -3,15 +3,20 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  console.log(req.rawHeaders)
   res.send("<h1>Hello</h1>");
 });
 
 app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+  res.send("<h1>Tentang Saya</h1><p>My name is Dary</p>");
 });
 
 app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+  res.send("<h1>Contact Me</h1><p>Phone: +62123456789</p>");
+});
+
+app.get("/links", (req, res) => {
+  res.send("<h1>My Links</h1><ul><li><a href='https://facebook.com/dary.reery' target='_blank'>Facebook</a></li></ul>")
 });
 
 app.listen(port, () => {
--- 18. ExpressJS/3.3-Postman/3.3 Postman/index.js
+++ 18. ExpressJS/3.3-Postman/3.3 Postman/index.js
@@ -18,7 +18,7 @@ app.post("/register", (req, res) => {
   res.sendStatus(201);
 });
 
-app.put("/user/angela", (req, res) => {
+app.put("/user/dary", (req, res) => {
   res.sendStatus(200);
 });
 
