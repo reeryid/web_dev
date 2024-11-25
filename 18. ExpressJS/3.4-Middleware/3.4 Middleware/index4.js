e93cefa3b69edabca1334d715e6596eb2f207cac
Dary <9desimeter@gmail.com>
2024-11-25 17:18:02 +0800

    Express JS Done

--- 18. ExpressJS/3.4-Middleware/3.4 Middleware/index4.js
+++ 18. ExpressJS/3.4-Middleware/3.4 Middleware/index4.js
@@ -1,7 +1,25 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/submit", (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.send(`<h1>Your band name is</h1><h1>${data.street} ${data.pet}</h1>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
